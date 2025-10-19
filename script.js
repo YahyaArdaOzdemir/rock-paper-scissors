fa0913d270c5f760153ddf81563ec6f1b34e2576
Yahya Arda Özdemir <153550965+YahyaArdaOzdemir@users.noreply.github.com>
2025-10-19 13:30:45 +0300

    Add Human Choice.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let computer
+let computer;
+let human;
+
 function getComputerChoice(){
     let x = Math.floor(Math.random()* 100) + 1
     if(x < 34){
@@ -14,3 +16,12 @@ function getComputerChoice(){
     return computer;
 }
 
+function getHumanChoice(){
+    let x = prompt("Will you have 'Rock', 'Paper' or 'Scissors', sire?")
+    human = x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase();
+    if(human !== "Rock" && human !=="Paper" && human !=="Scissors"){
+        return "Sire, I beseech thee. Choose either rock, paper or scissors."
+    }
+    console.log("You have made your choice...");
+    return human;
+}
