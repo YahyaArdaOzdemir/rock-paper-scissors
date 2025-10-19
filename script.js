44d29b4dd5f6d2a38953c5a521680c95fb0d8cb7
Yahya Arda Özdemir <153550965+YahyaArdaOzdemir@users.noreply.github.com>
2025-10-19 13:49:29 +0300

    Made it possible to play a single round.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let computer;
 let human;
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice(){
     let x = Math.floor(Math.random()* 100) + 1
@@ -12,7 +14,6 @@ function getComputerChoice(){
     else{
         computer = "Scissors"
     }
-    console.log(x);
     return computer;
 }
 
@@ -20,8 +21,31 @@ function getHumanChoice(){
     let x = prompt("Will you have 'Rock', 'Paper' or 'Scissors', sire?")
     human = x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase();
     if(human !== "Rock" && human !=="Paper" && human !=="Scissors"){
-        return "Sire, I beseech thee. Choose either rock, paper or scissors."
+        return "Sire, I beseech thee. Choose either rock, paper or scissors.\n"
     }
     console.log("You have made your choice...");
     return human;
+}
+
+function playRound(human, computer){
+    human = getHumanChoice();
+    computer = getComputerChoice();
+    if((human === "Rock" && computer === "Scissors") ||
+        (human === "Paper" && computer === "Rock") ||
+        (human === "Scissors" && computer === "Paper")){
+            humanScore++;
+            console.log("Humanity has chosen: " + human + " against " +computer);
+            console.log("Humanity is VICTORIOUS! Score: " + humanScore + " / " + computerScore);
+
+        }
+    else if (human === computer){
+        console.log("Humanity has chosen: " + human + " against " +computer);
+        console.log("How could this be? It's a DRAW! Score: " + humanScore + " / " + computerScore)
+    }
+    else{
+        computerScore++;
+        console.log("Humanity has chosen: " + human + " against " +computer);
+        console.log("Humanity has been DEFEATED! Score: " + humanScore + " / " + computerScore)
+    }
+
 }
